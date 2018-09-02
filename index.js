4f6659c7587ae68de7beb5c098b6eeb91613eeba
trinhngockhang <trinhngockhang1503@gmail.com>
2018-09-02 23:10:58 +0700

    first

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+app.set("view engine","ejs");
+app.set("vỉews","./views");
+app.use(express.static(__dirname + '/public'));
+const PORT = process.env.PORT || 3000;
+
+
+app.get('/',(req,res) =>{
+	res.render("trangchu.ejs");
+})
+
+
+
+app.listen(PORT, err => {
+	if (err) throw err;
+	console.log(`Server listening on ${PORT}`);
+});
